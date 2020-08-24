33706474ba43bed5aab3b8f911f4e7b2070d65d7
Valentin Stratan <intern.valentin.stratan@assist.ro>
2020-08-24 11:40:30 +0300

    backup

--- app/controllers/club.controller.js
+++ app/controllers/club.controller.js
@@ -55,288 +55,335 @@ exports.create = (req, res) => {
       }
 
     })
-
-
-
 }
-exports.update = (req, res) => {
-  const id = req.params.id;
 
+exports.createTest = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(422).json({ errors: errors.array() });
     return;
   }
-
-  Club.update(req.body, { where: { id: id } })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Club was updated successfully."
-        });
-      } else {
-        res.status(404).send({
-          message: `Cannot update Club with id=${id}. Maybe Club was not found!`
-        });
-      }
-    })
-}
-
-
-exports.findOne = (req, res) => {
-
-  const id = req.params.id;
-
-  Club.findByPk(id)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving Club with id=" + id
-      });
+  console.log('Start');
+  const club = await Club.findOne({ where: { name: req.body.name } });
+  if(club) {
+    res.status(404).send({
+      message: 'The club is already taken',
+      club: club
     });
-};
-
-exports.findDetails = (req, res) => {
-  const id = req.params.clubId;
-  var memberIds = [];
-  var response = {
-    members: [],
-    owner: [],
-    ownerClubs: [],
-    events: []
   }
-  Club.findByPk(id)
-    .then(club => {
-      User.findOne({
-        attributes: ['id', 'first_name', 'last_name'],
-        where: { id: club.ownerId }
-      })
-        .then(user => {
-          response.owner = user;
-          Club.findAll({
-            attributes: ['name'],
-            where: { ownerId: user.id }
-          })
-            .then(club => {
-              response.ownerClubs = club;
+  console.log('Club Found');
 
-              ClubMember.findAll({ where: { clubId: id } })
-                .then(member => {
-                  if (member) {
-                    for (let i = 0; i < member.length; i++) {
-                      memberIds.push(member[i].userId)
-                    }
-                  }
-                  User.findAll({
-                    attributes: ['first_name', 'last_name'],
-                    where: {
-                      id: { [Op.in]: memberIds }
-                    }
-                  }).then(user => {
-                    response.members = user;
-                    Event.findAll({
-                      attributes: { exclude: ['createdAt', 'updatedAt'] },
-                      where: { clubId: id }
-                    })
-                      .then(event => {
-                        response.events = event
-                        res.send(response);
-                      })
-
-                  })
-                })
-            })
-
-        })
-    })
-    .catch(err => {
-      res.status(404).send({
-        message: "Error retrieving Club with id=" + id
-      });
-    })
-}
-
-exports.findAll = (req, res) => {
-
-  Club.findAll({ attributes: ['id', 'name', 'ownerId', "sportId"] })
-    .then(data => {
-      res.status(200).send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving clubs."
-      });
-    });
-};
-
-
-exports.findAllWithMembers = async (req, res) => {
-  try {
-    const clubs = await Club.findAll({
-      attributes: ['id', 'name', 'ownerId', "sportId"]
-    })
-    const clubsId = clubs.map(club => club.id);
-    const clubsOwners = clubs.map(club => club.ownerId);
-    const clubMembers = await ClubMember.findAll({
-      where: {
-        clubId: clubsId
-      }
-    })
-
-    const users = await User.findAll();
-
-    let clubList = [];
-    for (let i = 0; i < clubs.length; i++) {
-      let club = clubs[i];
-      let coachName = users.find(owner => owner.id === club.ownerId);
-      coachName = coachName.first_name;
-      let membrii = clubMembers.filter(membru => membru.clubId === club.id);
-      let photoArray = [];
-      for (let j = 0; j < membrii.length; j++) {
-        photo = users.find(user => user.id === membrii[j].userId);
-        photo = `${req.protocol}://${req.headers.host}/${photo.profile_photo}`;
-        photoArray.push({ photo: photo });
-      }
-      clubList.push({ club, coachName, photoArray });
-    }
-    res.json(clubList);
+  const newClub = {
+    name: req.body.name,
+    ownerId: req.body.ownerId,
+    sportId: req.body.sportId
   }
-  catch (err) {
-    res.status(500).json({ message: err.message });
+  
+  Club.create(newClub);
+  console.log('Club Created');
+  const createdClubId = await Club.findOne({
+    where: {
+      name: req.body.name
+    },
+    attributes: ['id']
+  });
+  console.log('Club ID Found');
+  
+  let emailArray = req.body.emailArray;
+
+  const users = await User.findAll({where: {email: emailArray}});
+
+  console.log('');
+  for(let i =0; i < users.length; i++) {
+    ClubMember.create({
+      userId: users[i].id,
+      clubId: createClubId
+    })
   }
+
+  res.send({message: 'Succes'});
 }
 
 
-exports.search = (req, res) => {
+      exports.update = (req, res) => {
+        const id = req.params.id;
 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(422).json({ errors: errors.array() });
-    return;
-  }
-
-  const ownerId = req.body.ownerId;
-  const name = req.body.name;
-  const sportId = req.body.sportId;
-  if (ownerId && name) {
-    Club.findAll({
-      where: {
-        ownerId: ownerId,
-        name: name
-      }
-    })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error ocurred while retrieving clubs"
-        });
-      });
-  } else if (ownerId) {
-    Club.findAll({
-      where: {
-        ownerId: ownerId
-      }
-    })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error ocurred while retrieving clubs"
-        });
-      });
-  } else if (name) {
-    Club.findAll({
-      where: {
-        name: name
-      }
-    })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error ocurred while retrieving clubs"
-        });
-      });
-  } else {
-    res.status(404).send({ message: "You have to search by something!" })
-  }
-
-
-};
-exports.delete = (req, res) => {
-  const id = req.params.id;
-
-  Club.destroy({
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.status(200).send({
-          message: "Club was deleted successfully!"
-        });
-      } else {
-        res.send.status(204)({
-          message: `Cannot delete Club with id=${id}. Maybe Club was not found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete Club with id=" + id
-      });
-    });
-};
-
-exports.sendMails = (req, res) => {
-  let senderList = req.body.emailArray;
-  console.log('-----------------------------------');
-  console.log(senderList);
-
-  //email.send();
-}
-
-
-exports.validate = () => {
-  return [
-
-    body('sportId').exists().notEmpty().isInt(),
-    body('ownerId').optional().isInt(),
-    body('name').exists().notEmpty().isString()
-
-  ]
-}
-
-
-
-exports.validateUpdate = () => {
-  return [
-    body('sportId').optional().isInt(),
-    body('ownerId').optional().isInt(),
-    body('name').optional().isString().custom(value => {
-      return Club.findOne({ where: { name: value } }).then(club => {
-        if (club) {
-          throw new Error('Name already in use!!');
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          res.status(422).json({ errors: errors.array() });
+          return;
         }
-      })
-    })
 
-  ]
-}
-exports.validateSearch = () => {
-  return [
-    body('ownerId').optional().isInt(),
-    body('sportId').optional().isInt(),
-    body('name').optional().isString()
-  ]
-}
+        Club.update(req.body, { where: { id: id } })
+          .then(num => {
+            if (num == 1) {
+              res.send({
+                message: "Club was updated successfully."
+              });
+            } else {
+              res.status(404).send({
+                message: `Cannot update Club with id=${id}. Maybe Club was not found!`
+              });
+            }
+          })
+      }
+
+
+      exports.findOne = (req, res) => {
+
+        const id = req.params.id;
+
+        Club.findByPk(id)
+          .then(data => {
+            res.send(data);
+          })
+          .catch(err => {
+            res.status(500).send({
+              message: "Error retrieving Club with id=" + id
+            });
+          });
+      };
+
+      exports.findDetails = (req, res) => {
+        const id = req.params.clubId;
+        var memberIds = [];
+        var response = {
+          members: [],
+          owner: [],
+          ownerClubs: [],
+          events: []
+        }
+        Club.findByPk(id)
+          .then(club => {
+            User.findOne({
+              attributes: ['id', 'first_name', 'last_name'],
+              where: { id: club.ownerId }
+            })
+              .then(user => {
+                response.owner = user;
+                Club.findAll({
+                  attributes: ['name'],
+                  where: { ownerId: user.id }
+                })
+                  .then(club => {
+                    response.ownerClubs = club;
+
+                    ClubMember.findAll({ where: { clubId: id } })
+                      .then(member => {
+                        if (member) {
+                          for (let i = 0; i < member.length; i++) {
+                            memberIds.push(member[i].userId)
+                          }
+                        }
+                        User.findAll({
+                          attributes: ['first_name', 'last_name'],
+                          where: {
+                            id: { [Op.in]: memberIds }
+                          }
+                        }).then(user => {
+                          response.members = user;
+                          Event.findAll({
+                            attributes: { exclude: ['createdAt', 'updatedAt'] },
+                            where: { clubId: id }
+                          })
+                            .then(event => {
+                              response.events = event
+                              res.send(response);
+                            })
+
+                        })
+                      })
+                  })
+
+              })
+          })
+          .catch(err => {
+            res.status(404).send({
+              message: "Error retrieving Club with id=" + id
+            });
+          })
+      }
+
+      exports.findAll = (req, res) => {
+
+        Club.findAll({ attributes: ['id', 'name', 'ownerId', "sportId"] })
+          .then(data => {
+            res.status(200).send(data);
+          })
+          .catch(err => {
+            res.status(500).send({
+              message:
+                err.message || "Some error occurred while retrieving clubs."
+            });
+          });
+      };
+
+
+      exports.findAllWithMembers = async (req, res) => {
+        try {
+          const clubs = await Club.findAll({
+            attributes: ['id', 'name', 'ownerId', "sportId"]
+          })
+          const clubsId = clubs.map(club => club.id);
+          const clubsOwners = clubs.map(club => club.ownerId);
+
+          const clubMembers = await ClubMember.findAll({
+            where: {
+              clubId: clubsId
+            }
+          })
+
+          const users = await User.findAll();
+
+          let clubList = [];
+          for (let i = 0; i < clubs.length; i++) {
+            let club = clubs[i];
+            let coachName = users.find(owner => owner.id === club.ownerId);
+            coachName = coachName.first_name;
+            let membrii = clubMembers.filter(membru => membru.clubId === club.id);
+            let photoArray = [];
+            for (let j = 0; j < membrii.length; j++) {
+              photo = users.find(user => user.id === membrii[j].userId);
+              photo = `${req.protocol}://${req.headers.host}/${photo.profile_photo}`;
+              photoArray.push({ photo: photo });
+            }
+            clubList.push({ club, coachName, photoArray });
+          }
+          res.json(clubList);
+        }
+        catch (err) {
+          res.status(500).json({ message: err.message });
+        }
+      }
+
+
+      exports.search = (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          res.status(422).json({ errors: errors.array() });
+          return;
+        }
+
+        const ownerId = req.body.ownerId;
+        const name = req.body.name;
+        const sportId = req.body.sportId;
+        if (ownerId && name) {
+          Club.findAll({
+            where: {
+              ownerId: ownerId,
+              name: name
+            }
+          })
+            .then(data => {
+              res.send(data);
+            })
+            .catch(err => {
+              res.status(500).send({
+                message:
+                  err.message || "Some error ocurred while retrieving clubs"
+              });
+            });
+        } else if (ownerId) {
+          Club.findAll({
+            where: {
+              ownerId: ownerId
+            }
+          })
+            .then(data => {
+              res.send(data);
+            })
+            .catch(err => {
+              res.status(500).send({
+                message:
+                  err.message || "Some error ocurred while retrieving clubs"
+              });
+            });
+        } else if (name) {
+          Club.findAll({
+            where: {
+              name: name
+            }
+          })
+            .then(data => {
+              res.send(data);
+            })
+            .catch(err => {
+              res.status(500).send({
+                message:
+                  err.message || "Some error ocurred while retrieving clubs"
+              });
+            });
+        } else {
+          res.status(404).send({ message: "You have to search by something!" })
+        }
+
+
+      };
+      exports.delete = (req, res) => {
+        const id = req.params.id;
+
+        Club.destroy({
+          where: { id: id }
+        })
+          .then(num => {
+            if (num == 1) {
+              res.status(200).send({
+                message: "Club was deleted successfully!"
+              });
+            } else {
+              res.send.status(204)({
+                message: `Cannot delete Club with id=${id}. Maybe Club was not found!`
+              });
+            }
+          })
+          .catch(err => {
+            res.status(500).send({
+              message: "Could not delete Club with id=" + id
+            });
+          });
+      };
+
+      exports.sendMails = (req, res) => {
+        let senderList = req.body.emailArray;
+        console.log('-----------------------------------');
+        console.log(senderList);
+
+        //email.send();
+      }
+
+
+      exports.validate = () => {
+        return [
+
+          body('sportId').exists().withMessage('Not exist').notEmpty().withMessage('is Empty').isInt().withMessage('not Int'),
+          body('ownerId').optional().isInt(),
+          body('name').exists().withMessage('not exists').notEmpty().isString().withMessage('not string')
+
+        ]
+      }
+
+
+
+      exports.validateUpdate = () => {
+        return [
+          body('sportId').optional().isInt(),
+          body('ownerId').optional().isInt(),
+          body('name').optional().isString().custom(value => {
+            return Club.findOne({ where: { name: value } }).then(club => {
+              if (club) {
+                throw new Error('Name already in use!!');
+              }
+            })
+          })
+
+        ]
+      }
+      exports.validateSearch = () => {
+        return [
+          body('ownerId').optional().isInt(),
+          body('sportId').optional().isInt(),
+          body('name').optional().isString()
+        ]
+      }
 
--- app/routes/club.routes.js
+++ app/routes/club.routes.js
@@ -3,14 +3,16 @@ module.exports= app => {
     
     
     var router=require("express").Router();
-    router.post("/create",clubs.validate(),clubs.create, clubs.sendMails);
+    router.post("/create",clubs.validate(),clubs.create);
     router.put("/:id",clubs.validateUpdate(),clubs.update);
     router.get("/:id",clubs.findOne);
     router.get("/",clubs.findAll);
     router.get("/details/:clubId",clubs.findDetails);
     router.post("/search",clubs.validateSearch(),clubs.search);
     router.delete("/:id",clubs.delete);
-    
+
+    router.post("/createTest",clubs.validate(),clubs.createTest);
+
     router.get('/list/all', clubs.findAllWithMembers);
     app.use('/api/club',router); 
 }
